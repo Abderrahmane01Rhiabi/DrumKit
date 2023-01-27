96e819a0590b34b0cda0a5dbb95b2f75812ec7f6
Abderrahmane01Rhiabi <rhiabi.abderrahmane@gmail.com>
2023-01-27 18:28:25 +0100

    Dramekit keyboard version

--- index.js
+++ index.js
@@ -2,6 +2,80 @@
 
 var buttons = document.querySelectorAll(".drum");
 
+
+//Detecting button press
+
+document.addEventListener("click", (event) => {
+    console.log(event);
+});
+
+//adding to a specific element (button)
+for (let i = 0; i < buttons.length; i++) {
+
+        buttons[i].addEventListener("click", function (event) {
+            
+            console.log(this.innerHTML);
+            //console.log(event);
+            makeSound(this.innerHTML);
+
+    })
+}
+
+//Detecting keyboard press
+
+//adding to the entier document
+document.addEventListener("keydown", function (event) { 
+
+            console.log(event);
+            makeSound(event.key);
+
+});
+
+function makeSound(key){
+
+    switch (key) {
+        case "w":
+            var audio = new Audio("sounds/crash.mp3");
+            audio.play();
+            break;
+
+        case "a":
+            var audio = new Audio("sounds/kick-bass.mp3");
+            audio.play();
+            break;
+            
+        case "s":
+            var audio = new Audio("sounds/snare.mp3");
+            audio.play();
+            break;
+
+        case "d":
+            var audio = new Audio("sounds/tom-1.mp3");
+            audio.play();
+            break;
+
+        case "j":
+            var audio = new Audio("sounds/tom-2.mp3");
+            audio.play();
+            break;
+        case "k":
+
+            var audio = new Audio("sounds/tom-3.mp3");
+            audio.play();
+            break;
+        case "l":
+
+            var audio = new Audio("sounds/tom-4.mp3");
+            audio.play();
+            break;
+
+        default:
+            break;
+    }
+
+}
+
+/*
 for (let i = 0; i < buttons.length; i++) {
 
     buttons[i].addEventListener("click", function () {
@@ -56,3 +130,49 @@ for (let i = 0; i < buttons.length; i++) {
     });
 }
 
+document.addEventListener("keydown", function (event) {
+        
+    console.log(this);
+
+        switch (event.key) {
+            case "w":
+                var audio = new Audio("sounds/crash.mp3");
+                audio.play();
+                break;
+
+            case "a":
+                var audio = new Audio("sounds/kick-bass.mp3");
+                audio.play();
+                break;
+                
+            case "s":
+                var audio = new Audio("sounds/snare.mp3");
+                audio.play();
+                break;
+
+            case "d":
+                var audio = new Audio("sounds/tom-1.mp3");
+                audio.play();
+                break;
+
+            case "j":
+                var audio = new Audio("sounds/tom-2.mp3");
+                audio.play();
+                break;
+            case "k":
+
+                var audio = new Audio("sounds/tom-3.mp3");
+                audio.play();
+                break;
+            case "l":
+
+                var audio = new Audio("sounds/tom-4.mp3");
+                audio.play();
+                break;
+
+            default:
+                console.log(event.key);
+        }
+
+    });
+*/
